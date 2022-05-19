9043bdc9eb78d7d3fc7ea06a3ad9600ef6f39033
Mozetsu <hello@mozetsu.dev>
2022-05-19 23:59:25 +0100

    chore: add app context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-import "./index.css";
+import theme from "./theme/index";
+import { AppProvider } from "./context/AppContext";
+import "./theme/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider>
+  <ChakraProvider theme={theme}>
+    <AppProvider>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+    </AppProvider>
+  </ChakraProvider>,
 );
