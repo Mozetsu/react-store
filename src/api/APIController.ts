cbf04df649659452d5322625c00fa2829116743c
Mozetsu <hello@mozetsu.dev>
2022-05-20 16:16:14 +0100

    refactor: update API controller

--- src/api/APIController.ts
+++ src/api/APIController.ts
@@ -1,44 +1,23 @@
 import axios from "axios";
+import priceToNumber from "../lib";
+import { ItemInterface } from "../interfaces";
 
 const BASE_URL: string = "http://localhost:8081/store";
 
-interface QueriedItem {
-  name: string;
-  price: string;
-  type: string;
-}
-
-interface ParsedItem {
-  name: string;
-  price: number;
-  type: string;
-}
-
-// convert price property to number
-const priceToNumber = (itemsArray: QueriedItem[]): ParsedItem[] => {
-  const parsedItems = itemsArray.map((item) => {
-    const parsedPrice: number = parseInt(item.price.split("$")[0], 10);
-    return { ...item, price: parsedPrice };
-  });
-
-  return parsedItems;
-};
-
 export default {
-  getAll: async () => {
-    const { data } = await axios.get<QueriedItem[]>(`${BASE_URL}/parts`);
+  // fetch all items from API
+  getAll: async (): Promise<ItemInterface<number>[]> => {
+    const { data } = await axios.get(`${BASE_URL}/parts`);
     return priceToNumber(data);
   },
-  getByType: async (type: string) => {
-    const { data } = await axios.get<QueriedItem[]>(
-      `${BASE_URL}/parts?type=${type}`,
-    );
+  // fetch items by type
+  getByType: async (type: string): Promise<ItemInterface<number>[]> => {
+    const { data } = await axios.get(`${BASE_URL}/parts?type=${type}`);
     return priceToNumber(data);
   },
-  getByQuery: async (query: string) => {
-    const { data } = await axios.get<QueriedItem[]>(
-      `${BASE_URL}/parts?query=${query}`,
-    );
+  // fetch items by query (input)
+  getByQuery: async (query: string): Promise<ItemInterface<number>[]> => {
+    const { data } = await axios.get(`${BASE_URL}/parts?query=${query}`);
     return priceToNumber(data);
   },
 };
