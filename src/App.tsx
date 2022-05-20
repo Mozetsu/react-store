a2f63198133eb6734470a318ad110dc2cf5df4a3
Mozetsu <hello@mozetsu.dev>
2022-05-20 16:20:36 +0100

    feat: add middleware to parts route

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // pages
 import Home from "./pages/Home";
 import Parts from "./pages/Parts";
+import NotFound from "./pages/NotFound";
+
+// components
+import Middleware from "./components/Middleware";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/parts" element={<Parts />} />
+        <Route
+          path="/parts"
+          element={
+            <Middleware>
+              <Parts />
+            </Middleware>
+          }
+        />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
